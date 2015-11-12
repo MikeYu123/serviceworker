7637a40b7c2af0f0f40a2c781ffd82205e24a28d
MikeYu123 <mayurchenkov@edu.hse.ru>
2015-11-12 17:43:47 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,10 +2,6 @@ console.log("I am a Service Worker");
 
 this.addEventListener('install', function(e) {
   console.log("I am a Service Worker and I have been installed");
-  Notification.requestPermission(function(permission){
-  document.permission = permission;
-  console.log(permission);
-});
 });
 
 this.addEventListener('activate', function(e) {

65f9de3833388c14c02f84eb520952f22c732128
MikeYu123 <mayurchenkov@edu.hse.ru>
2015-11-12 17:30:32 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,8 @@
 console.log("I am a Service Worker");
+Notification.requestPermission(function(permission){
+// переменная permission содержит результат запроса
+  document.permission = permission
+});
 
 this.addEventListener('install', function(e) {
   console.log("I am a Service Worker and I have been installed");

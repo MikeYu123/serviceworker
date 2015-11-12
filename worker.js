d1f180df239a4e7e1ee3aa0822f52d3cdfd2dd1a
MikeYu123 <mayurchenkov@edu.hse.ru>
2015-11-12 17:49:40 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,4 +6,17 @@ this.addEventListener('install', function(e) {
 
 this.addEventListener('activate', function(e) {
   console.log("I am a Service Worker and I have been activated");
+    self.registration.showNotification("Go back to serviceworker", {
+    actions: [{action: 'archive', title: "Archive"}]
+  });
+  
+  self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    if (event.action === 'archive') {
+      var win = window.open('https://mikeyu123.github.io/serviceworker/', '_blank');
+      win.focus();
+    } else {
+      clients.openWindow("https://mikeyu123.github.io/serviceworker_first/");
+    }
+  }, false);
 });

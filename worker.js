387344491c2458605cd5623498fc5af2fbdbc6f4
MikeYu123 <mayurchenkov@edu.hse.ru>
2015-11-13 09:47:01 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -7,45 +7,21 @@ this.addEventListener('install', function(e) {
 this.addEventListener('activate', function(e) {
   console.log("I am a Service Worker and I have been activated");
    a = function(){
-    self.registration.showNotification("Go back to serviceworker", {
-      actions: [{action: 'archive', title: "Archive"}]
-    });
-    setTimeout(a,5000);
+    var req = new XMLHttpRequest();
+    req.open('GET', 'https://mikeyu123.github.io/serviceworker/');
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        self.registration.showNotification("Go back to serviceworker", {
+          actions: [{action: 'archive', title: "Archive"}]
+        });
+      setTimeout(a,5000);
+      }
+    }
     };
   a();
 
-  
-  self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    if (event.action === 'archive') {
-      var win = window.open('https://mikeyu123.github.io/serviceworker/', '_blank');
-      win.focus();
-    } else {
-      clients.openWindow("https://mikeyu123.github.io/serviceworker/");
-    }
-  }, false);
-});
-
-this.addEventListener('fetch', function(e) {
-  self.registration.showNotification("Go back to serviceworker", {
-    actions: [{action: 'archive', title: "Archive"}]
-  });
-  
-  self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    if (event.action === 'archive') {
-      var win = window.open('https://mikeyu123.github.io/serviceworker/', '_blank');
-      win.focus();
-    } else {
-      clients.openWindow("https://mikeyu123.github.io/serviceworker/");
-    }
-  }, false);
-});
-
-this.addEventListener('message', function(e) {
-  self.registration.showNotification("Go back to serviceworker", {
-    actions: [{action: 'archive', title: "Archive"}]
-  });
   
   self.addEventListener('notificationclick', function(event) {
     event.notification.close();
